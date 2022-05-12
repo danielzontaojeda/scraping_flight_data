f1b2f4a8f114f78c1f6c44f4eb643f2c986cec18
danielzontaojeda <danielzontaojeda@gmail.com>
2022-05-12 13:33:19 -0300

    implemented close privacy settings, click advanced search

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print("hello world")
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://www.flightstats.com/v2")
+
+# waits for privacy window to appear
+time.sleep(3)
+
+# clicks I Accept
+privacySettings = driver.find_element_by_id("onetrust-accept-btn-handler")
+privacySettings.click()
+
+#clicks Advanced Search
+advancedSearch = driver.find_element_by_class_name("flight-tracker-adv-search-button")
+advancedSearch.click()
+
+#inserts IGU at Arrival Airport Field
+arrivalAirport = driver.find_element_by_name("arrivalAirport")
+arrivalAirport.send_keys("IGU")
+time.sleep(1)
+arrivalAirport.send_keys(Keys.ARROW_DOWN, Keys.ENTER)
+time.sleep(1)
+
+#clicks search
+search = driver.find_element_by_class_name("basic-button__Button-sc-3qdr1i-0 kmYwtt")
+search.click()
